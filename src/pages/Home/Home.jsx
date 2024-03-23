1ac8909923cd9a133dd7d06107f54ce9c5c7ebfe
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-23 09:51:12 +0100

    created menu items

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
 import Header from '../../component/Header/Header'
 import './Home.css'
 
 const Home = () => {
+
+    const[category, setCategory] = useState('All');
+
+
+
   return (
     <div>
         <Header/>
+        <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
